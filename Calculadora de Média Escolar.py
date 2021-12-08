60df9e00085cc997d300c6630d5c805944bf9ca2
Lucas Forti Santi <90848720+fortilucas@users.noreply.github.com>
2021-12-08 12:37:20 -0300

    Add files via upload

---
+++ Calculadora de Média Escolar.py
@@ -0,0 +1,42 @@
+n1=float(input('Digite sua N1: '))
+if n1>10:
+    n1=float(input('Nota maior que 10. Digite sua n1 novamente: '))
+n2=float(input('Digite sua N2: '))
+if n2>10:
+    n2=float(input('Nota maior que 10. Digite sua n2 novamente: '))
+n3=float(input('Digite sua N3: '))
+if n3>10:
+    n3 = float(input('Nota maior que 10. Digite sua n3 novamente: '))
+n4=float(input('Digite sua N4: '))
+if n4>10:
+    n4=float(input('Nota maior que 10. Digite sua n4 novamente: '))
+media=(n1+n2+n3+n4)/4
+print('sua média é: {}'.format(media))
+
+
+
+
+
+# if n1<=10 and n2<=10 and n3<=10 and n4<=10:
+#     print ('media: {}'.format(media))
+# else:
+#     print('alguma nota errada')
+
+
+
+
+
+
+
+# a=int(input('primeiro valor: '))
+# b=int(input('segundo valor: '))
+# c=int(input('terceiro valor: '))
+# if a>b and a>c:
+#     print('o maior número é {}'.format(a))
+# elif b>a and b>c :
+#     print('o maior número é {}'.format(b))
+# else c > a and c > b:
+#     print('o maior número é {}'.format(c))
+#
+#
+# print('fim')
